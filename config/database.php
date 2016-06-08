7a93c86e89cb2b3a4285abb38363768139859d80
Christian Hernandez <christian.hernandez@redhat.com>
2016-06-08 14:39:29 -0700

    updated db connection to use port

--- config/database.php
+++ config/database.php
@@ -1,6 +1,7 @@
 <?php
 // used to connect to the database
 $host = getenv("MYSQL_SERVICE_HOST");
+$port = getenv("MYSQL_SERVICE_PORT");
 $db_name = getenv("MYSQL_DATABASE");
 $username = getenv("MYSQL_USER");
 $password = getenv("MYSQL_PASSWORD");
@@ -9,7 +10,7 @@ $password = getenv("MYSQL_PASSWORD");
 
 
 try {
-	$con = new PDO("mysql:host={$host};dbname={$db_name}", $username, $password);
+	$con = new PDO("mysql:host={$host};dbname={$db_name};port={$port}", $username, $password);
 }
 
 // to handle connection error
