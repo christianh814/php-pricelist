479fb314f71978ad1c34fb6b750e58dba2e185c4
Christian Hernandez <christian.hernandez@yahoo.com>
2017-12-04 15:23:37 -0800

    added configfile support with /data/db.php

--- config/database.php
+++ config/database.php
@@ -1,10 +1,20 @@
 <?php
-// used to connect to the database
-$host = getenv("MYSQL_SERVICE_HOST");
-$port = getenv("MYSQL_SERVICE_PORT");
-$db_name = getenv("MYSQL_DATABASE");
-$username = getenv("MYSQL_USER");
-$password = getenv("MYSQL_PASSWORD");
+// Define configfile path
+$db_config_file = "/data/db.php";
+//
+if (file_exists(file_exists($filename))) {
+	// include config file if we have found it
+	require_once($db_config_file);
+	$using_config_file = "yes";
+} else {
+	// If file isn't there, we'll assume you want env vars
+	$host = getenv("MYSQL_SERVICE_HOST");
+	$port = getenv("MYSQL_SERVICE_PORT");
+	$db_name = getenv("MYSQL_DATABASE");
+	$username = getenv("MYSQL_USER");
+	$password = getenv("MYSQL_PASSWORD");
+	$using_config_file = "no";
+}
 
 try {
 	$con = new PDO("mysql:host={$host};dbname={$db_name};port={$port}", $username, $password);
