425da1693b8c0218d13833ee2fd622dbd70d733c
Christian Hernandez <christian.hernandez@yahoo.com>
2018-05-16 14:38:10 -0700

    Create np test

--- index.php
+++ index.php
@@ -1,1 +1,28 @@
-read.php
+<?php
+// include core configuration
+include 'config/core.php';
+ 
+// include database connection
+//include 'config/database.php';
+
+// action variable
+$action = isset($_GET['action']) ? $_GET['action'] : "";
+
+// page header
+$page_title="Pricelist";
+include_once "layout_head.php";
+?>
+<a href='create.php' class='btn btn-primary pull-right margin-bottom-1em'>
+<span class='glyphicon glyphicon-plus'></span> Create Record
+</a>
+<a href='read.php' class='btn btn-primary pull-right margin-bottom-1em'>
+        <span class='glyphicon glyphicon-list'></span> Read Records
+</a>
+
+<?php
+// include the read template
+//include_once "read_template.php";
+
+// page footer
+include_once "layout_foot.php";
+?>
