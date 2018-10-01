f8fe431f7a891a58c871e57e874c958216630b59
Christian Hernandez <christianh814@users.noreply.github.com>
2018-10-01 13:34:03 -0700

    Update create_database.php

--- create_database.php
+++ create_database.php
@@ -1,10 +1,20 @@
 <?php
-// used to connect to the database
-$host = getenv("MYSQL_SERVICE_HOST");
-$port = getenv("MYSQL_SERVICE_PORT");
-$db_name = getenv("MYSQL_DATABASE");
-$username = getenv("MYSQL_USER");
-$password = getenv("MYSQL_PASSWORD");
+// Define configfile path
+$db_config_file = "/data/db.php";
+//
+if (file_exists($db_config_file)) {
+	// include config file if we have found it
+	require_once($db_config_file);
+	$using_config_file = "yes";
+} else {
+	// If file isn't there, we'll assume you want env vars
+	$host = getenv("MYSQL_SERVICE_HOST");
+	$port = getenv("MYSQL_SERVICE_PORT");
+	$db_name = getenv("MYSQL_DATABASE");
+	$username = getenv("MYSQL_USER");
+	$password = getenv("MYSQL_PASSWORD");
+	$using_config_file = "no";
+}
 
 try {
 	$sql = "CREATE TABLE IF NOT EXISTS `products` (
