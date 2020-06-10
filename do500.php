ddc9b8daf5ff614a5d29ff781f8c26f1798c9f1a
Christian Hernandez <christian.hernandez@yahoo.com>
2020-06-10 12:29:53 -0700

    fixing 500 error text

--- do500.php
+++ do500.php
@@ -12,11 +12,11 @@ include_once "layout_head.php";
 // to identify page for paging
 $page_url="do500.php?";
 
-// include the read template
-include_once "index_template.php";
-
 // send back 500 code
 http_response_code(500);
+
+// include the read template
+include_once "error_template.php";
 
 // page footer
 include_once "layout_foot.php";
---
+++ error_template.php
@@ -0,0 +1,32 @@
+<?php 
+echo "<div class='overflow-hidden'>";
+	?>
+	<form role="search" action='search.php'>
+		<div class="input-group col-md-3 pull-left">
+			<input type="text" class="form-control" placeholder="Type a name..." name="s" id="srch-term" required <?php echo isset($search_term) ? "value='$search_term'" : ""; ?> />
+			<div class="input-group-btn">
+				<button class="btn btn-primary" type="submit"><i class="glyphicon glyphicon-search"></i></button>
+			</div>
+		</div>
+	</form>
+	<?php 
+	echo "<a href='create.php' class='btn btn-primary pull-right margin-bottom-1em'>";
+		echo "<span class='glyphicon glyphicon-plus'></span> Create Record";
+	echo "</a>";
+	
+	echo "<a href='export_csv.php' class='btn btn-info pull-right margin-bottom-1em margin-right-1em'>";
+		echo "<span class='glyphicon glyphicon-download'></span> Export CSV";
+	echo "</a>";
+	
+	echo "<a href='read.php' class='btn btn-primary pull-right margin-bottom-1em margin-right-1em'>";
+		echo "<span class='glyphicon glyphicon-list'></span> Read Records";
+	echo "</a>";
+	
+echo "</div>";
+
+// Body of Index Page
+echo "<div class='alert alert-danger' role='alert'>";
+	echo "<p>Internal Server Error</p>"
+	echo "<br>";
+echo "</div>";
+?>
